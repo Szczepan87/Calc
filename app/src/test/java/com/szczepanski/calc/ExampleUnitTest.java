7e8bd49fa10d3fb0e4f08e58b0377b69e6c37a10
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-24 14:36:31 +0200

    problem z działaniami polega na czytaniu wartości dziesiętnych, zamiast przecinka w formacie liczby powininna być kropka (wynika to chyba z ustawień regionalnych)

--- app/src/test/java/com/szczepanski/calc/ExampleUnitTest.java
+++ app/src/test/java/com/szczepanski/calc/ExampleUnitTest.java
@@ -2,6 +2,8 @@ package com.szczepanski.calc;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,9 +11,17 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest{
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void multiplyTest(){
+        BigDecimal firstVal = BigDecimal.valueOf(Double.valueOf("12.0"));
+        BigDecimal secondVal = BigDecimal.valueOf(Double.valueOf("10.0"));
+        BigDecimal result = firstVal.multiply(secondVal);
+        assertEquals(120.0,result);
     }
 }
