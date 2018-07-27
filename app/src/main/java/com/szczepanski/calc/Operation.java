b061dcaacbbd38108e36aa649b3af1d10d889d36
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-27 13:24:53 +0200

    zmiana metody setOperator - ignoruje pierwszy minus

--- app/src/main/java/com/szczepanski/calc/Operation.java
+++ app/src/main/java/com/szczepanski/calc/Operation.java
@@ -14,14 +14,23 @@ public class Operation implements BasicCalcOperations {
     }
 
     private void setOperator(String value) {
-        if (value.contains("+"))
-            this.operator = "+";
-        else if (value.contains("-"))
-            this.operator = "-";
-        else if (value.contains("*"))
-            this.operator = "*";
-        else if (value.contains("/"))
-            this.operator = "/";
+        if (value.charAt(0) == '-') {
+            if (value.contains("+"))
+                this.operator = "+";
+            else if (value.contains("*"))
+                this.operator = "*";
+            else if (value.contains("/"))
+                this.operator = "/";
+        } else {
+            if (value.contains("+"))
+                this.operator = "+";
+            else if (value.contains("-"))
+                this.operator = "-";
+            else if (value.contains("*"))
+                this.operator = "*";
+            else if (value.contains("/"))
+                this.operator = "/";
+        }
     }
 
     private String cutZeroes(String input) {
@@ -29,8 +38,8 @@ public class Operation implements BasicCalcOperations {
             return input;
         else {
             for (int i = input.length() - 1; i >= 0; i--) {
-                if (input.charAt(input.length()-1)=='.' || input.charAt(input.length() - 1) == ','){
-                    input = String.valueOf(new StringBuilder(input).deleteCharAt(input.length()-1));
+                if (input.charAt(input.length() - 1) == '.' || input.charAt(input.length() - 1) == ',') {
+                    input = String.valueOf(new StringBuilder(input).deleteCharAt(input.length() - 1));
                     break;
                 }
                 if (input.charAt(i) != '0') {
@@ -85,7 +94,7 @@ public class Operation implements BasicCalcOperations {
         if (split[1].equals("0") || split[1].equals("") || split[1] == null)
             return "ERR!";
         firstVal = BigDecimal.valueOf(Double.parseDouble(split[0]));
-        result = firstVal.divide(BigDecimal.valueOf(Double.parseDouble(split[1])),10,RoundingMode.HALF_UP);
+        result = firstVal.divide(BigDecimal.valueOf(Double.parseDouble(split[1])), 10, RoundingMode.HALF_UP);
         return cutZeroes(String.valueOf(result));
     }
 }
