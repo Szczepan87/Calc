a7200792b6f65ad81391999f15ab890a2aa2c715
Łukasz Szczepański <l.szczepan@wp.pl>
2018-07-30 12:44:43 +0200

    dodanie zabezpieczenia przed wartościami nieliczbowymi

--- app/src/main/java/com/szczepanski/calc/Display.java
+++ app/src/main/java/com/szczepanski/calc/Display.java
@@ -4,6 +4,9 @@ public class Display {
 
     private final char[] operators = new char[]{'+', '-', '*', '/'};
 
+    private final char[] properInput = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            ',', '.', '+', '-', '*', '/'};
+
     public boolean isLastCharAnOperator(String input) {
         for (char operator : operators) {
             if (input.charAt(input.length() - 1) == operator)
@@ -12,13 +15,34 @@ public class Display {
         return false;
     }
 
-    public boolean isZero(String input){
+    public boolean isProperInput(String input) {
+        char[] inputArr = input.toCharArray();
+        int counter = 0;
+        for (int i = 0; i < inputArr.length; i++) {
+            for (int j = 0; j < properInput.length; j++) {
+                if (inputArr[i] == properInput[j])
+                    counter++;
+            }
+        }
+        if (counter == input.length())
+            return true;
+        else return false;
+    }
+
+    public boolean isZero(String input) {
         return input.equals("0");
     }
 
-    public String updateDisplay(String onScreen, String value){
-        onScreen += value;
-
+    public String updateDisplay(String onScreen, String value) {
+        if (isProperInput(value)) {
+            if (isZero(onScreen)) {
+                onScreen = onScreen.replace("0", value);
+            } else if (isLastCharAnOperator(onScreen) && isLastCharAnOperator(value)) {
+                onScreen = onScreen.replace(String.valueOf(onScreen.charAt(onScreen.length() - 1)), value);
+            } else {
+                onScreen += value;
+            }
+        }
         return onScreen;
     }
 }
--- app/src/test/java/com/szczepanski/calc/DisplayTest.java
+++ app/src/test/java/com/szczepanski/calc/DisplayTest.java
@@ -11,27 +11,27 @@ public class DisplayTest {
     private Display display = new Display();
 
     @Test
-    public void checkingOperatorPlus() {
+    public void checkingOperatorPlusTest() {
         assertTrue(display.isLastCharAnOperator("123+"));
     }
 
     @Test
-    public void checkingOperatorMinus() {
+    public void checkingOperatorMinusTest() {
         assertTrue(display.isLastCharAnOperator("123-"));
     }
 
     @Test
-    public void checkingOperatorMultiply() {
+    public void checkingOperatorMultiplyTest() {
         assertTrue(display.isLastCharAnOperator("123*"));
     }
 
     @Test
-    public void checkingOperatorDivide() {
+    public void checkingOperatorDivideTest() {
         assertTrue(display.isLastCharAnOperator("123/"));
     }
 
     @Test
-    public void checkingNonOperator() {
+    public void checkingNonOperatorTest() {
         assertFalse(display.isLastCharAnOperator("123"));
     }
 
@@ -47,36 +47,41 @@ public class DisplayTest {
 
     @Test
     public void updateDisplayByOperatorTest() {
-        assertEquals("123+", display.updateDisplay("123","+"));
+        assertEquals("123+", display.updateDisplay("123", "+"));
     }
 
     @Test
     public void updateDisplayByNumberTest() {
-        assertEquals("123", display.updateDisplay("12","3"));
+        assertEquals("123", display.updateDisplay("12", "3"));
     }
 
     @Test
     public void updateDisplayByZeroTest() {
-        assertEquals("0", display.updateDisplay("0","0"));
+        assertEquals("0", display.updateDisplay("0", "0"));
     }
 
     @Test
     public void updateDisplayByNumberWithZeroOnScreenTest() {
-        assertEquals("9", display.updateDisplay("0","9"));
+        assertEquals("9", display.updateDisplay("0", "9"));
     }
 
     @Test
     public void updateDisplayByTwoSameOperatorsTest() {
-        assertEquals("123+", display.updateDisplay("123+","+"));
+        assertEquals("123+", display.updateDisplay("123+", "+"));
     }
 
     @Test
     public void updateDisplayByTwoOperatorsTest() {
-        assertEquals("123+", display.updateDisplay("123-","-"));
+        assertEquals("123-", display.updateDisplay("123+", "-"));
     }
 
     @Test
     public void updateDisplayBySecondValueTest() {
-        assertEquals("123+456", display.updateDisplay("123+","456"));
+        assertEquals("123+456", display.updateDisplay("123+", "456"));
+    }
+
+    @Test
+    public void checkWrongInputTest() {
+        assertEquals("123+", display.updateDisplay("123+", "abc"));
     }
 }
