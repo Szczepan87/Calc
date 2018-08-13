0fd28cb6e93d6f857916b232256a437f09695a61
Łukasz Szczepański <l.szczepan@wp.pl>
2018-08-13 15:12:11 +0200

    stworzenie szablonu dla jednostek miary

---
+++ app/src/main/java/com/szczepanski/calc/UnitOfMesurement.java
@@ -0,0 +1,29 @@
+package com.szczepanski.calc;
+
+import java.math.BigDecimal;
+
+public class UnitOfMesurement{
+
+    private BigDecimal value;
+    private String fullName;
+    private String shortName;
+
+    public UnitOfMesurement(final BigDecimal value, final String fullName, final String shortName) {
+        this.value = value;
+        this.fullName = fullName;
+        this.shortName = shortName;
+    }
+
+
+    public BigDecimal getUnitValue() {
+        return this.value;
+    }
+
+    public String getShortName() {
+        return this.shortName;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+}
