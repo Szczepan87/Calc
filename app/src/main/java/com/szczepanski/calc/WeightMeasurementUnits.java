bb4f72973c372d79cc13ce3b947f33645211813f
Łukasz Szczepański <l.szczepan@wp.pl>
2018-08-14 17:08:44 +0200

    korekta szablonu dla jednostek miar, usunięcie publicznej metody uzupełaniającej listę, klasa jednostek miar długości uzupełniona o konstruktor bezparametroay

---
+++ app/src/main/java/com/szczepanski/calc/WeightMeasurementUnits.java
@@ -0,0 +1,18 @@
+package com.szczepanski.calc;
+
+import java.math.BigDecimal;
+
+public class WeightMeasurementUnits extends UnitDatabaseTemplate {
+    private final UnitOfMeasurement gram = new UnitOfMeasurement(BigDecimal.valueOf(1),"Gram", "g");
+
+    private void fillListOfMeasurementUnits() {
+        getListOfMeasurementUnits().add(gram);
+        getListOfMeasurementUnits().add(new UnitOfMeasurement(gram.getUnitValue().multiply(BigDecimal.valueOf(5)),"Carat(metric)","CD"));
+        getListOfMeasurementUnits().add(new UnitOfMeasurement(gram.getUnitValue().multiply(BigDecimal.valueOf(10)),"Decagram","dag"));
+        getListOfMeasurementUnits().add(new UnitOfMeasurement(gram.getUnitValue().multiply(BigDecimal.valueOf(28.349523)),"Ounce","oz"));
+        getListOfMeasurementUnits().add(new UnitOfMeasurement(gram.getUnitValue().multiply(BigDecimal.valueOf(31.103477)),"Ounce(precious metals)","ozt"));
+        getListOfMeasurementUnits().add(new UnitOfMeasurement(gram.getUnitValue().multiply(BigDecimal.valueOf(453.59237)),"Pounds","lbs"));
+        getListOfMeasurementUnits().add(new UnitOfMeasurement(gram.getUnitValue().multiply(BigDecimal.valueOf(1000)),"Kilogram","kg"));
+
+    }
+}
