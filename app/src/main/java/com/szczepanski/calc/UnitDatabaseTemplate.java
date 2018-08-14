57e7b652ca8773b1fb01ac3dc2b37f7fdaa1bdb3
Łukasz Szczepański <l.szczepan@wp.pl>
2018-08-14 15:29:55 +0200

    tworzenie szablonu dla klas obsługujących jednostki miar

---
+++ app/src/main/java/com/szczepanski/calc/UnitDatabaseTemplate.java
@@ -0,0 +1,26 @@
+package com.szczepanski.calc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class UnitDatabaseTemplate {
+
+
+    private List<UnitOfMeasurement> listOfMeasurementUnits = new ArrayList<>();
+
+    public abstract void fillListOfMeasurementUnits();
+
+    public List<UnitOfMeasurement> getListOfMeasurementUnits() {
+        return listOfMeasurementUnits;
+    }
+
+    public UnitOfMeasurement getDistanceUnit(String fullNameOrShortName) {
+        for (int i = 0; i < listOfMeasurementUnits.size(); i++) {
+            if (listOfMeasurementUnits.get(i).getFullName().toLowerCase().equals(fullNameOrShortName))
+                return listOfMeasurementUnits.get(i);
+            else if (listOfMeasurementUnits.get(i).getShortName().toLowerCase().equals(fullNameOrShortName))
+                return listOfMeasurementUnits.get(i);
+        }
+        return null;
+    }
+}
